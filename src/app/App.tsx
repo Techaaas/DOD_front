497b67bbc92d9e11bc609a0158925feefe734c79
Дарьяно <dashastepanova1901@mail.ru>
2023-11-19 01:12:59 +0300

    added scanner

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Auth from "../components/authorization/auth";
 import Header from "../components/header/header";
+import QrScan from "../components/QrScan/QrScan";
 
 function App() {
   return (
       <div>
           <Header/>
-          <Auth/>
+          {/*<Auth/>*/}
+          <QrScan></QrScan>
       </div>
   );
 }
