020ea7cb8be18e2936556231a6edbf846917b2eb
dehopen <a2302004b23@yandex.ru>
2023-11-20 05:16:39 +0300

    Update 1.6 Setting routes for pages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import './App.css';
-import InfoGuest from "../components/info_about_gest/InfoGuest";
+import InfoGuest from "../pages/InfoGuest/InfoGuest";
+import {Route, Routes} from "react-router-dom";
+import TakePhoto from "../components/info_about_gest/take_photo/TakePhoto";
 // import TakePhoto from "../components/info_about_gest/take_photo/TakePhoto";
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
       <div className="App">
-        {/*<Router>*/}
-        {/*  <Routes>*/}
-        {/*    <Route path="/take-photo" element={<TakePhoto />} />*/}
-        {/*    /!* Добавьте другие маршруты здесь *!/*/}
-        {/*  </Routes>*/}
-        {/*</Router>*/}
-        <InfoGuest visible={true} name={'Andrey'} surname={'Gerasimov'}></InfoGuest>
+          <Routes>
+            <Route element = {<InfoGuest visible={true} name={'Andrey'} surname={'Gerasimov'}></InfoGuest>}>
+              <Route path="/take_photo" element={<TakePhoto></TakePhoto>}/>
+            </Route>
+          </Routes>
+
         {/*<TakePhoto></TakePhoto>*/}
       </div>
   );
